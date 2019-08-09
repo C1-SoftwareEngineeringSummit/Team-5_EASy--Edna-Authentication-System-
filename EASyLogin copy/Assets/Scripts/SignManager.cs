32cd1bb70286b5c0ce7a7754469823031141ee39
ben-gillott <bg357@cornell.edu>
2019-08-09 09:59:01 -0400

    All files

---
+++ EASyLogin copy/Assets/Scripts/SignManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+// using static PhotoUploadT;
+
+
+public class SignManager : MonoBehaviour
+{
+    //  //Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space")){
+            // ScreenCapture.CaptureScreenshot("Screenshots/Signature.jpg");
+            SceneManager.LoadSceneAsync("Loading_Scene1", LoadSceneMode.Single);
+        }
+
+        //TODO: Uncomment to for 
+        // if (Input.GetKeyDown("z")){
+        //     PhotoUploadT p = new PhotoUploadT();
+        //     p.CallThisInPhotoUpload();
+        // }
+    }
+}
